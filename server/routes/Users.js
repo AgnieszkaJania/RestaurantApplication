f3d779177dc42688e8aa2d7dd409c4a949c1aa70
AgnieszkaJania <65187859+AgnieszkaJania@users.noreply.github.com>
2022-03-20 19:38:29 +0100

    changes Users get requests and added user login

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -4,13 +4,18 @@ const { Users } = require('../models')
 const bcrypt = require('bcrypt');
 
 router.get("/", async (req,res)=>{
-    const listOfUsers = await Users.findAll();
+    const listOfUsers = await Users.findAll({
+        attributes:['firstName','lastName','phoneNumber','email']
+    });
     res.json(listOfUsers);
 });
 
 router.get("/:id",async (req,res)=>{
     const id = req.params.id;
-    const user = await Users.findByPk(id);
+    const user = await Users.findOne({
+        attributes:{exclude: ['userPassword']},
+        where: {id:id}
+    });
     res.json(user);
 })
 
@@ -27,5 +32,21 @@ router.post("/register", async (req,res)=>{
         res.json("User added")
     });
 });
+
+router.post("/login", async (req,res)=>{
+    const {email, userPassword} = req.body;
+    const user = await Users.findOne({where:{email:email}});
+    if(!user){
+        res.json({error:"User does not exist!"})
+    }else{
+        bcrypt.compare(userPassword,user.userPassword).then((match)=>{
+            if(!match){
+                res.json({error:"Wrong username or password!"});
+            }else{
+                res.json("You are logged in!");
+            }
+        })
+    }
+})
 
 module.exports = router
