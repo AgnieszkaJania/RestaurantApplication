77eae8dfc7b2e3bbe5c3713638971cdf261cb89a
AgnieszkaJania <65187859+AgnieszkaJania@users.noreply.github.com>
2022-11-19 19:30:40 +0100

    function to find full booking data by booking id

---
+++ server/helpers/Bookings.js
@@ -0,0 +1,26 @@
+const { Users, Bookings, Statuses,Tables,Restaurants } = require('../models')
+
+async function findBookingFullDataByBookingId(id){
+    const booking = await Bookings.findOne({
+        where:{id:id},
+        include:[
+            {
+                model:Tables,
+                required:true,
+                include:[{
+                    model:Restaurants,
+                    required:true
+                }]
+            },
+            {
+                model: Statuses,
+                required:true
+            }
+        ]
+    });
+    return booking
+}
+
+module.exports = {
+    findBookingFullDataByBookingId:findBookingFullDataByBookingId
+}
