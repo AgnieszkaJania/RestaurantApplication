4e8e1c015092c27f25e0dbc013e27265c0c679c6
AgnieszkaJania <65187859+AgnieszkaJania@users.noreply.github.com>
2022-12-04 12:10:24 +0100

    seeding function moved to separate folder, typo fixed

---
+++ server/dbservices/seedDatabase.js
@@ -0,0 +1,29 @@
+const {Statuses, Cuisines} = require('../models')
+
+async function seedDatabase(){
+    await Statuses.bulkCreate([
+        {status: "Available"},
+        {status: "Booked"},
+        {status: "Disabled"},
+    ],{ignoreDuplicates: true});
+    await Cuisines.bulkCreate([
+        {cuisineName: "Burgers"},
+        {cuisineName:"Italian"},
+        {cuisineName:"Polish"},
+        {cuisineName:"German"},
+        {cuisineName:"Pizza"},
+        {cuisineName:"Steak"},
+        {cuisineName:"Asian"},
+        {cuisineName:"Sushi"},
+        {cuisineName:"Korean"},
+        {cuisineName:"Fusion"},
+        {cuisineName:"France"},
+        {cuisineName:"Hungary"},
+        {cuisineName:"Ramen"},
+        {cuisineName:"Bao"},
+    ],{ignoreDuplicates:true});
+}
+
+module.exports = {
+    seedDatabase:seedDatabase
+}
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
+const {seedDatabase} = require('./dbservices/seedDatabase')
 const app = express();
 
 app.use(express.json());
@@ -26,34 +27,8 @@ app.use("/bookings", bookingRouter);
 const cuisineRouter = require('./routes/Cuisines');
 app.use('/cuisines', cuisineRouter);
 
-const {Statuses, Cuisines} = require('./models');
-
-async function seedDatabse(){
-    const statuses = await Statuses.bulkCreate([
-        {status: "Available"},
-        {status: "Booked"},
-        {status: "Disabled"},
-    ],{ignoreDuplicates: true});
-    const cuisines = await  Cuisines.bulkCreate([
-        {cuisineName: "Burgers"},
-        {cuisineName:"Italian"},
-        {cuisineName:"Polish"},
-        {cuisineName:"German"},
-        {cuisineName:"Pizza"},
-        {cuisineName:"Steak"},
-        {cuisineName:"Asian"},
-        {cuisineName:"Sushi"},
-        {cuisineName:"Korean"},
-        {cuisineName:"Fusion"},
-        {cuisineName:"France"},
-        {cuisineName:"Hungary"},
-        {cuisineName:"Ramen"},
-        {cuisineName:"Bao"},
-    ],{ignoreDuplicates:true});
-}
-
 db.sequelize.sync().then(()=>{
-   seedDatabse().then(()=> console.log("Database synchronized and seeded!"))
+   seedDatabase().then(()=> console.log("Database synchronized and seeded!"))
 });
 
 
