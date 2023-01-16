3f08611e3914a66947d9b12de94dcc56b61e0125
AgnieszkaJania <65187859+AgnieszkaJania@users.noreply.github.com>
2023-01-16 02:07:42 +0100

    refactored addidng cuisines to restaurants and list all cuisines

--- server/routes/Cuisines.js
+++ server/routes/Cuisines.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const router = express.Router()
-const {validateRestaurantToken} = require('../middlewares/AuthMiddleware')
+const express = require('express');
+const router = express.Router();
+const {validateRestaurantToken} = require('../middlewares/AuthMiddleware');
 const { body, validationResult } = require('express-validator');
-const { Op } = require("sequelize");
+const { Op } = require('sequelize');
 const { Cuisines, RestaurantsCuisines } = require('../models');
+const{getAllCuisines, getCuisinesAssignedToRestaurant, addCuisinesToRestaurant} = require('../services/Cuisines');
 
 // API endpoint to add cuisine to restaurant
 
@@ -11,79 +12,83 @@ router.post("/add",
 body('cuisines').not().isEmpty().withMessage('No data!'),
 validateRestaurantToken,
 async (req,res)=>{
+    try {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(422).json({added: false, error: errors.array()[0].msg});
+        };
 
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(422).json({added: false, error: errors.array()[0].msg})
-    };
-    const cuisines = req.body.cuisines;
+        const restaurantId = req.restaurantId;
+        const cuisinesToAdd = req.body.cuisines;
 
-    const allCuisines = await Cuisines.findAll({
-        attributes:['id']
-    })
-    let arrAllCuisines = []
-    for(let i = 0; i < allCuisines.length; i++){
-        arrAllCuisines.push(allCuisines[i].id)
-    }
-    let incorrectData = false
-    cuisines.forEach(element => {
-        if(!arrAllCuisines.includes(element)){
-            incorrectData = true
+        const allCuisines = await getAllCuisines();
+        const allCuisinesIds = Array.from(allCuisines, x => x.id);
+        let incorrectData = false;
+        
+        cuisinesToAdd.forEach(element => {
+            if(!allCuisinesIds.includes(element)){
+                incorrectData = true;
+            }
+        });
+        if(incorrectData){
+            return res.status(400).json({added:false, error:"Incorrect data! Trying to add data which does not exist in the system!"});
         }
-    });
-    if(incorrectData){
-        return res.status(400).json({added:false, error:"Incorrect data!"})
-    }
 
-    const cuisinesForRestaurant = await RestaurantsCuisines.findAll({
-       where:{RestaurantId:req.restaurantId}
-    })
-    let arrCuisinesForRestaurant = []
-    for(let i = 0; i<cuisinesForRestaurant.length; i++){
-        arrCuisinesForRestaurant.push(cuisinesForRestaurant[i].CuisineId)
-    }
-    let duplicateData = false
-    cuisines.forEach(element =>{
-        if(arrCuisinesForRestaurant.includes(element)){
-            duplicateData = true
+        const cuisinesAssignedToRestaurant = await getCuisinesAssignedToRestaurant(restaurantId);
+        const cuisinesAssignedToRestaurantIds = Array.from(cuisinesAssignedToRestaurant, x => x.CuisineId);
+        let duplicateData = false;
+
+        cuisinesToAdd.forEach(element => {
+            if(cuisinesAssignedToRestaurantIds.includes(element)){
+                duplicateData = true;
+            }
+        })
+        if(duplicateData){
+            return res.status(400).json({added:false, error:"Incorrect data! Trying to add duplicates!"});
         }
-    })
-    if(duplicateData){
-        return res.status(400).json({added:false, error:"Incorrect data! Trying to add duplicates!"})
+
+        const addedCuisinesToRestaurant = await addCuisinesToRestaurant(cuisinesToAdd, restaurantId);
+        return res.status(201).json({added: true, cuisines: addedCuisinesToRestaurant});
+
+    } catch (error) {
+        return res.status(400).json({added: false, error:error});
     }
-
-    let cuisinesList = []
-    for(let i = 0; i < cuisines.length; i++){
-        let cuisineRestaurant = {CuisineId: cuisines[i],
-            RestaurantId: req.restaurantId
-        }
-        cuisinesList.push(cuisineRestaurant)
-    }
-
-    RestaurantsCuisines.bulkCreate(cuisinesList).then(()=> {
-        res.status(200).json(cuisinesList)
-    }).catch((err)=>{
-        if(err){
-            res.status(400).json({error:err})
-        }
-    });
-
 });
 
 // API endpoint to list all cuisines available
 
-router.get("/all",async (req,res)=>{
-
-    const cuisines = await Cuisines.findAll();
-    res.status(200).json(cuisines);
+router.get("/all", async (req,res)=>{
+    try {
+        const allCuisines = await getAllCuisines();
+        return res.status(200).json(allCuisines);
+    } catch (error) {
+        return res.status(400).json({error:error});
+    }
 });
 
-// API endpoint to get all cuisines for a restaurant
+// API endpoint to get all cuisines for a logged in restaurant
 
 router.get("/", validateRestaurantToken, async (req,res)=>{
 
     const cuisines = await RestaurantsCuisines.findAll({
         where:{RestaurantId:req.restaurantId},
+        include:[{
+            model:Cuisines,
+            required:true
+        }]
+    });
+    if(cuisines.length == 0){
+        return res.status(200).json({message:"Your restaurant does not have any cuisine assigned!"})
+    }
+    res.status(200).json(cuisines);
+});
+
+// API endpoint to get all cuisines for a  restaurant
+
+router.get("/:restaurantId", async (req,res)=>{
+
+    const cuisines = await RestaurantsCuisines.findAll({
+        where:{RestaurantId:req.params.restaurantId},
         include:[{
             model:Cuisines,
             required:true
--- server/services/Cuisines.js
+++ server/services/Cuisines.js
@@ -1,10 +1,39 @@
-const { Cuisine } = require('../models');
+const { Cuisine, RestaurantCuisine } = require('../models');
 
 async function checkIfCuisinesExist(){
     const allCuisines = await Cuisine.findAll();
     return allCuisines.length > 0;
 }
 
+async function getAllCuisines(){
+    const allCuisines = await Cuisine.findAll();
+    return allCuisines;
+}
+
+async function getCuisinesAssignedToRestaurant(restaurantId){
+    const cuisinesAssignedToRestaurant = await RestaurantCuisine.findAll({
+        where:{RestaurantId: restaurantId}
+    });
+    return cuisinesAssignedToRestaurant;
+}
+
+async function addCuisinesToRestaurant(cuisinesList, restaurantId){
+    let cuisineRestaurantObjectList = [];
+    cuisinesList.forEach(element => {
+        let cuisineRestaurantObject = {
+            CuisineId: element,
+            RestaurantId: restaurantId
+        }
+        cuisineRestaurantObjectList.push(cuisineRestaurantObject);
+    });
+    const addedCuisinesToRestaurant = await RestaurantCuisine.bulkCreate(cuisineRestaurantObjectList);
+    return addedCuisinesToRestaurant;
+}
+
 module.exports = {
-    checkIfCuisinesExist:checkIfCuisinesExist
+    checkIfCuisinesExist:checkIfCuisinesExist,
+    getAllCuisines: getAllCuisines,
+    getCuisinesAssignedToRestaurant: getCuisinesAssignedToRestaurant,
+    addCuisinesToRestaurant: addCuisinesToRestaurant
+
 }
