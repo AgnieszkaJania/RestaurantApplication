302d5874aeabe4782f0113a0bae95c178456d924
AgnieszkaJania <65187859+AgnieszkaJania@users.noreply.github.com>
2023-01-25 00:50:57 +0100

    removed unused middleware to validate restore token, changed statuses

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -4,7 +4,7 @@ const validateToken = (req,res,next) => {
     const accessToken = req.cookies["access-token"];
 
     if(!accessToken){
-        return res.status(400).json({auth: false, error: "User not logged in!"});
+        return res.status(401).json({auth: false, error: "User not logged in!"});
     }else{
         jwt.verify(accessToken,"veryimportantsecretmessage", (error, decoded)=>{
             if(error){
@@ -19,11 +19,12 @@ const validateToken = (req,res,next) => {
     }
 
 };
+
 const validateRestaurantToken = (req,res,next) => {
     const accessToken = req.cookies["access-token-restaurant"];
 
     if(!accessToken){
-        return res.status(400).json({auth: false, error: "Restaurant not logged in!"});
+        return res.status(401).json({auth: false, error: "Restaurant not logged in!"});
     }else{
         jwt.verify(accessToken,"veryimportantsecretmessageforrestaurant", (error, decoded)=>{
             if(error){
@@ -38,22 +39,4 @@ const validateRestaurantToken = (req,res,next) => {
 
 };
 
-const validateRestoreToken = (req,res,next) => {
-    const restoreToken = req.cookies["restore-token"];
-
-    if(!restoreToken){
-        return res.status(400).json({auth: false, error: "Session ended. Try again!"});
-    }else{
-        jwt.verify(restoreToken,"restore", (error, decoded)=>{
-            if(error){
-                res.json({restored: false, error: "Failed to autenticate!"})
-            }else{
-                req.userEmail = decoded.email;
-                return next();
-            }
-        });
-    }
-
-};
-
-module.exports = {validateToken, validateRestaurantToken,validateRestoreToken};
+module.exports = {validateToken, validateRestaurantToken};
